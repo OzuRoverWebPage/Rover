97a0dfdf53d5a110c6bfeac319a80dbdfc664b22
cengizhanozcan <cengizhan.ozcan@ozu.edu.tr>
2015-10-07 23:11:16 +0300

    All codes are added

---
+++ WebstormProjects/untitled1/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by cengizhanozcan on 02/10/15.
+ */
+
+$(document).ready(function() {
+    $('.menuBar').mouseenter(function() {
+        $(this).css("color","blue");
+    });
+    $('.menuBar').mouseleave(function () {
+        $(this).css("color","blue");
+    });
+
+});
